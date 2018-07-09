e2e8116d796430b006c6c81d34f013a0b1656838
abhiram-vedullapalli <abhiram.vedu@anywhere.co>
2018-07-09 15:05:40 +0530

    create, string case conversion , age problem , inputmismatch exception
    user was given a option to create his own custom players(previously
    players were predefined) , age was fixed to 40 morethan 40 players will
    not be selected into team , inputmismatch exception was handled , user
    can enter his input in any case. function was defined to convert the
    string to a particular order(Starting letter of each word in a string is
    capital letter)

--- src/practice/Crud.java
+++ src/practice/Crud.java
@@ -3,105 +3,189 @@ package practice;
 import java.util.*;
 import practice.Details;
 
-public class Crud {
+public class Crud extends StringConversion {
+	static HashMap<String, Details> players = new HashMap<>();
+	static Crud play = new Crud();
 
 	public static void main(String[] args) {
+
+		displayMenu();
+	}
+
+	public static void displayMenu() {
 		Scanner a = new Scanner(System.in);
-		Details sachin = new Details("Sachin", "mi", 45);
-		Details dhoni = new Details("Dhoni", "csk", 35);
-		Details kohli = new Details("Kohli","rcb",28);
-
-		HashMap<String, Details> player = new HashMap<>();
-
-		//Adding player details
-		player.put(sachin.getName(), sachin);
-		player.put(dhoni.getName(), dhoni);
-		player.put(kohli.getName(), kohli);
 		boolean check = false;
 		do {
 			System.out.println("MENU \n");
-			System.out.println("Retrieve Player : 1");
-			System.out.println("Update a Player : 2");
-			System.out.println("Delete a Player : 3");
-			System.out.println("Print    Players: 4");
+			System.out.println("Create a Player : 1");
+			System.out.println("Retrieve Player : 2");
+			System.out.println("Update a Player : 3");
+			System.out.println("Delete a Player : 4");
+			System.out.println("List all Players: 5");
 			System.out.print("\n Please Enter your Choice : ");
-			int input = a.nextInt();
-			switch (input) {
-			case 1:
-				retrievePlayer(player);
-				break;
-			case 2:
-				updatePlayer(player);
-				break;
-			case 3:
-				deletePlayer(player);
-				break;
-			case 4:
-				System.out.println("All Player Records " + player);
+
+			try {
+				int input = a.nextInt();
+				switch (input) {
+				case 1:
+					createPlayer();
+					break;
+				case 2:
+					retrievePlayer();
+					break;
+				case 3:
+					updatePlayer();
+					break;
+				case 4:
+					deletePlayer();
+					break;
+				case 5:
+					listAllPlayers();
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Option you entered is not present in the Menu. Enter a valid option");
+				displayMenu();
 			}
+
 			String empty = a.nextLine();
-			System.out.println("Do you want to continue: Enter 'y' for Yes and 'n' for Exit :");
+			System.out.println("Do you want to perform another operation: Enter 'y' for Yes and 'n' for Exit :");
 			String yesNo = a.nextLine();
-			
+
 			if (yesNo.equals("y")) {
 				check = false;
-			} if(yesNo.equals("n")) {
+			}
+			if (yesNo.equals("n")) {
 				check = true;
 				System.out.println("Thank you");
 			}
 		} while (check == false);
 	}
 
-	public static void retrievePlayer(HashMap<String, Details> m) {
+	public static void createPlayer() {
+		Scanner sc = new Scanner(System.in);
+		boolean check = false;
+		do {
+
+			System.out.print("Enter Name of Player : ");
+			String noCaseName = sc.nextLine();
+			String name = play.anyCase(noCaseName);
+			System.out.print("Enter Team of Player : ");
+			String noCaseTeam = sc.nextLine();
+			String team = play.anyCase(noCaseTeam);
+			System.out.print("Enter Age of Player : ");
+			int age = sc.nextInt();
+			if (age > 40) {
+				System.out.println("\n Players whose age is above 40 are not selected \n");
+				System.out.print("Enter Age again : ");
+				int age1 = sc.nextInt();
+				age = age1;
+			}
+			String empty = sc.nextLine();
+
+			players.put(name, new Details(name, team, age));
+			System.out.println("Do you want to create another Player : If Yes enter 'y' else 'n' ");
+			String yesNo = sc.nextLine();
+
+			if (yesNo.equals("y")) {
+				check = false;
+			}
+			if (yesNo.equals("n")) {
+				check = true;
+			}
+		} while (check == false);
+
+	}
+
+	public static void retrievePlayer() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the name of player you want to retrieve : ");
-		String search = sc.nextLine();
-		if (m.containsKey(search)) {
-			System.out.println(m.get(search));
+		String noCaseSearch = sc.nextLine();
+		String search = play.anyCase(noCaseSearch);
+		if (players.containsKey(search)) {
+			System.out.println(players.get(search));
 		} else {
 			System.out.println("Player not present . Invalid Player");
 		}
 	}
 
-	public static void updatePlayer(HashMap<String, Details> m) {
+	public static void updatePlayer() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the name of the player you want to update the details : ");
-		String up = sc.nextLine();
-		if (m.containsKey(up)) {
+		String noCaseUpdate = sc.nextLine();
+		String update = play.anyCase(noCaseUpdate);
+		if (players.containsKey(update)) {
 			System.out.println("To change Team name of Player: 1");
 			System.out.println("To change   Age     of Player: 2");
 			System.out.print("\n Enter your choice : ");
-			int choice = sc.nextInt();
-			String empty = sc.nextLine();
-			switch (choice) {
+			try {
+				int choice = sc.nextInt();
+				String empty = sc.nextLine();
+				switch (choice) {
 
-			case 1:
-				System.out.print("Enter new team name of Player : ");
-				String newTeam = sc.nextLine();
-				m.get(up).setTeam(newTeam);
-				break;
-			case 2:
-				System.out.print("Enter new Age of Player : ");
-				int newAge = sc.nextInt();
-				m.get(up).setAge(newAge);
+				case 1:
+					System.out.print("Enter new team name of Player : ");
+					String newTeam = sc.nextLine();
+					players.get(update).setTeam(newTeam);
+					break;
+				case 2:
+					System.out.print("Enter new Age of Player : ");
+					int newAge = sc.nextInt();
+					if (newAge > 40) {
+						System.out.println("Players whose age is 40 or greater than 40 are retired");
+						System.out.print("Enter Age again : ");
+						int age = sc.nextInt();
+						newAge = age;
+					}
+					players.get(update).setAge(newAge);
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Option you entered is invalid. Enter the option in the Menu");
+				updatePlayer();
 			}
+		} else {
+			System.out.println("Player not present. Invalid player");
 		}
-		else {
+
+	}
+
+	public static void deletePlayer() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the name of Player you want to delete : ");
+		String noCaseDelPlayer = sc.nextLine();
+		String delPlayer = play.anyCase(noCaseDelPlayer);
+		if (players.containsKey(delPlayer)) {
+			players.remove(delPlayer);
+			System.out.println("Records of player " + delPlayer + " deleted");
+		} else {
 			System.out.println("Player doesn't exist. Enter a valid name");
 		}
 	}
 
-	public static void deletePlayer(HashMap<String, Details> m) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the name of Player you want to delete : ");
-		String delPlayer = sc.nextLine();
-		if(m.containsKey(delPlayer)) {
-			m.remove(delPlayer);
-			System.out.println("Records of player " + delPlayer + " deleted");
+	public static void listAllPlayers() {
+		System.out.println("\n DETAILS OF ALL PLAYERS \n");
+		for (HashMap.Entry<String, Details> entry : players.entrySet()) {
+			System.out.println(entry.getValue());
+
 		}
-		else {
-			System.out.println("Player doesn't exist. Enter a valid name");
+	}
+
+	@Override
+	// whatever the case , the user may give his inputs.
+	// this function converts input such that (Starting letter of every word in
+	// string is in upper case)
+	// this helps while retrieving and updating the data from the Hashmap
+	String anyCase(String s) {
+		char[] c = s.toCharArray();
+		c[0] = Character.toUpperCase(s.charAt(0));
+		for (int i = 0; i < s.length(); i++) {
+			if (c[i] == ' ') {
+				c[i + 1] = Character.toUpperCase(c[i + 1]);
+			}
 		}
+		String modified = String.valueOf(c);
+		return modified;
+
 	}
 
 }
---
+++ src/practice/StringConversion.java
@@ -0,0 +1,5 @@
+package practice;
+
+public abstract class StringConversion {
+abstract  String anyCase(String s);
+}
